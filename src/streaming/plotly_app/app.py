def8ebfa2128f2b0a44e549ccae3da227de1034d
philiphaddad97 <philiphaddad97@gmail.com>
2022-12-04 23:15:55 +0100

    fix dash example

--- src/streaming/plotly_app/app.py
+++ src/streaming/plotly_app/app.py
@@ -43,36 +43,6 @@ def update_graph_scatter(n):
             yaxis=dict(range=[min(Y), max(Y)]),
         ),
     }
-import influxdb_client
-from influxdb_client.client.write_api import SYNCHRONOUS
-
-class Influx:
-    def __init__(self) -> None:
-        self.token = "91XonVTK0cXpXmwjoF32jKOeMKIkf2LPK4Vm5lURZJKR2mNP9pftxRxvcBu487wUa0hPoggNTImKDFpzzxDQsQ=="
-        self.url = "http://influxdb:8086"
-        self.org = "ost"
-        self.client = influxdb_client.InfluxDBClient(
-        url=self.url,
-        token=self.token,
-        org=self.org)
-
-    def get_data(self):
-        # Query script
-        query_api = self.client.query_api()
-        query = 'from(bucket:"my-bucket")\
-        |> range(start: -10m)\
-        |> filter(fn:(r) => r._measurement == "my_measurement")\
-        |> filter(fn:(r) => r.location == "Prague")\
-        |> filter(fn:(r) => r._field == "temperature")'
-        result = query_api.query(org=self.org, query=query)
-        results = []
-        for table in result:
-            for record in table.records:
-                results.append((record.get_field(), record.get_value()))
-
-        print(results)
-
 
 if __name__ == "__main__":
-    # app.run_server(debug=True)
-    
+    app.run(host="0.0.0.0", port="8050")
